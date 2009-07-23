08f7398e0adaa520a2e6f64cb3c364f57c014712
Mark Bates <mark@markbates.com>
2009-07-23 13:56:40 -0400

    Removed clean_token method.

--- lib/apn_on_rails/app/models/apn/device.rb
+++ lib/apn_on_rails/app/models/apn/device.rb
@@ -8,22 +8,9 @@ class APN::Device < ActiveRecord::Base
   def token=(token)
     res = token.scan(/\<(.+)\>/).first
     unless res.nil? || res.empty?
-      # self.token = res.first
       token = res.first
     end
     write_attribute('token', token)
-  end
-  
-  before_validation :clean_token
-  
-  private
-  def clean_token
-    if self.token && self.token_changed?
-      res = self.token.scan(/\<(.+)\>/).first
-      unless res.nil? || res.empty?
-        self.token = res.first
-      end
-    end
   end
   
 end
